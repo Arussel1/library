5b70ce5c911d18ca0b634d7b4c644595ebea1003
Arussel <anhminh05102004@gmail.com>
2024-04-28 20:33:31 -0400

    Add function for book display

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-const myLibrary = [];
+const myLibrary = [new Book("Harry Potter","Rowling",400,false), new Book("Harry Potter","Rowling",300,false), new Book("Harry Potter","Rowling",500,false)];
 
-function Book() {
-  name: this.name;
-  author: this.author
+function Book(name,author,pages,read) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  let bookName = document.forms["addBook"].name.value;
+  let author = document.forms["addBook"].author.value;
+  let page = parseInt(document.forms["addBook"].page.value);
+  let read = document.forms["addBook"].read.value == true;
+  myLibrary.push(new Book(bookName,author,page,read));
 }
+
+function displayBook(lib){
+  let bookContainer = document.querySelector(".bookContainer");
+  for(let book of lib){
+    let div = document.createElement("div");
+    div.innerHTML = 
+    `<p>${book.name}<\p>` +   `<p>${book.author}<\p>` + `<p>${book.pages}<\p>`  +`<p>${book.read}<\p>` ;
+    bookContainer.appendChild(div);
+  }
+} 
+displayBook(myLibrary);
