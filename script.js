5e1bd26f5f6e3d468be775503308f4fde5822bcd
Arussel <anhminh05102004@gmail.com>
2024-05-01 19:43:32 -0400

    add function for displaying new book

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+// Book array
 const myLibrary = [new Book("Harry Potter","Rowling",400,false), new Book("Harry Potter","Rowling",300,false), new Book("Harry Potter","Rowling",500,false)];
-
+// define a Book
 function Book(name,author,pages,read) {
   this.name = name;
   this.author = author;
@@ -15,13 +16,28 @@ function addBookToLibrary() {
   myLibrary.push(new Book(bookName,author,page,read));
 }
 
-function displayBook(lib){
+function displayBook(){
   let bookContainer = document.querySelector(".bookContainer");
-  for(let book of lib){
+  for(let book of myLibrary){
     let div = document.createElement("div");
     div.innerHTML = 
     `<p>${book.name}<\p>` +   `<p>${book.author}<\p>` + `<p>${book.pages}<\p>`  +`<p>${book.read}<\p>` ;
     bookContainer.appendChild(div);
   }
 } 
-displayBook(myLibrary);
+displayBook();
+// open + close dialog
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+  addBookToLibrary();
+  displayBook();
+  dialog.close();
+  event.preventDefault();
+});
